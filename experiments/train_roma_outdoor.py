b4634b989a68a0080cc19f4da029835b8bfdd6ef
tjelinek <tomasjelinek96@gmail.com>
2025-05-26 12:10:53 +0200

    reformat

--- experiments/train_roma_outdoor.py
+++ experiments/train_roma_outdoor.py
@@ -23,10 +23,10 @@ from romatch.models.transformer import Block, TransformerDecoder, MemEffAttentio
 from romatch.models.encoders import *
 from romatch.checkpointing import CheckPoint
 
-resolutions = {"low":(448, 448), "medium":(14*8*5, 14*8*5), "high":(14*8*6, 14*8*6)}
+resolutions = {"low": (448, 448), "medium": (14 * 8 * 5, 14 * 8 * 5), "high": (14 * 8 * 6, 14 * 8 * 6)}
 
 
-def get_model(pretrained_backbone=True, resolution = "medium", checkpoint_path=None, **kwargs):
+def get_model(pretrained_backbone=True, resolution="medium", checkpoint_path=None, **kwargs):
     import warnings
     warnings.filterwarnings('ignore', category=UserWarning, message='TypedStorage is deprecated')
     gp_dim = 512
@@ -34,77 +34,77 @@ def get_model(pretrained_backbone=True, resolution = "medium", checkpoint_path=N
     decoder_dim = gp_dim + feat_dim
     cls_to_coord_res = 64
     coordinate_decoder = TransformerDecoder(
-        nn.Sequential(*[Block(decoder_dim, 8, attn_class=MemEffAttention) for _ in range(5)]), 
-        decoder_dim, 
-        cls_to_coord_res**2 + 1,
+        nn.Sequential(*[Block(decoder_dim, 8, attn_class=MemEffAttention) for _ in range(5)]),
+        decoder_dim,
+        cls_to_coord_res ** 2 + 1,
         is_classifier=True,
-        amp = True,
-        pos_enc = False,)
+        amp=True,
+        pos_enc=False, )
     dw = True
     hidden_blocks = 8
     kernel_size = 5
     displacement_emb = "linear"
     disable_local_corr_grad = True
-    
+
     conv_refiner = nn.ModuleDict(
         {
             "16": ConvRefiner(
-                2 * 512+128+(2*7+1)**2,
-                2 * 512+128+(2*7+1)**2,
+                2 * 512 + 128 + (2 * 7 + 1) ** 2,
+                2 * 512 + 128 + (2 * 7 + 1) ** 2,
                 2 + 1,
                 kernel_size=kernel_size,
                 dw=dw,
                 hidden_blocks=hidden_blocks,
                 displacement_emb=displacement_emb,
                 displacement_emb_dim=128,
-                local_corr_radius = 7,
-                corr_in_other = True,
-                amp = True,
-                disable_local_corr_grad = disable_local_corr_grad,
-                bn_momentum = 0.01,
+                local_corr_radius=7,
+                corr_in_other=True,
+                amp=True,
+                disable_local_corr_grad=disable_local_corr_grad,
+                bn_momentum=0.01,
             ),
             "8": ConvRefiner(
-                2 * 512+64+(2*3+1)**2,
-                2 * 512+64+(2*3+1)**2,
+                2 * 512 + 64 + (2 * 3 + 1) ** 2,
+                2 * 512 + 64 + (2 * 3 + 1) ** 2,
                 2 + 1,
                 kernel_size=kernel_size,
                 dw=dw,
                 hidden_blocks=hidden_blocks,
                 displacement_emb=displacement_emb,
                 displacement_emb_dim=64,
-                local_corr_radius = 3,
-                corr_in_other = True,
-                amp = True,
-                disable_local_corr_grad = disable_local_corr_grad,
-                bn_momentum = 0.01,
+                local_corr_radius=3,
+                corr_in_other=True,
+                amp=True,
+                disable_local_corr_grad=disable_local_corr_grad,
+                bn_momentum=0.01,
             ),
             "4": ConvRefiner(
-                2 * 256+32+(2*2+1)**2,
-                2 * 256+32+(2*2+1)**2,
+                2 * 256 + 32 + (2 * 2 + 1) ** 2,
+                2 * 256 + 32 + (2 * 2 + 1) ** 2,
                 2 + 1,
                 kernel_size=kernel_size,
                 dw=dw,
                 hidden_blocks=hidden_blocks,
                 displacement_emb=displacement_emb,
                 displacement_emb_dim=32,
-                local_corr_radius = 2,
-                corr_in_other = True,
-                amp = True,
-                disable_local_corr_grad = disable_local_corr_grad,
-                bn_momentum = 0.01,
+                local_corr_radius=2,
+                corr_in_other=True,
+                amp=True,
+                disable_local_corr_grad=disable_local_corr_grad,
+                bn_momentum=0.01,
             ),
             "2": ConvRefiner(
-                2 * 64+16,
-                128+16,
+                2 * 64 + 16,
+                128 + 16,
                 2 + 1,
                 kernel_size=kernel_size,
                 dw=dw,
                 hidden_blocks=hidden_blocks,
                 displacement_emb=displacement_emb,
                 displacement_emb_dim=16,
-                amp = True,
-                disable_local_corr_grad = disable_local_corr_grad,
-                bn_momentum = 0.01,
+                amp=True,
+                disable_local_corr_grad=disable_local_corr_grad,
+                bn_momentum=0.01,
             ),
             "1": ConvRefiner(
                 2 * 9 + 6,
@@ -112,12 +112,12 @@ def get_model(pretrained_backbone=True, resolution = "medium", checkpoint_path=N
                 2 + 1,
                 kernel_size=kernel_size,
                 dw=dw,
-                hidden_blocks = hidden_blocks,
-                displacement_emb = displacement_emb,
-                displacement_emb_dim = 6,
-                amp = True,
-                disable_local_corr_grad = disable_local_corr_grad,
-                bn_momentum = 0.01,
+                hidden_blocks=hidden_blocks,
+                displacement_emb=displacement_emb,
+                displacement_emb_dim=6,
+                amp=True,
+                disable_local_corr_grad=disable_local_corr_grad,
+                bn_momentum=0.01,
             ),
         }
     )
@@ -148,26 +148,26 @@ def get_model(pretrained_backbone=True, resolution = "medium", checkpoint_path=N
         "4": proj4,
         "2": proj2,
         "1": proj1,
-        })
+    })
     displacement_dropout_p = 0.0
     gm_warp_dropout_p = 0.0
-    decoder = Decoder(coordinate_decoder, 
-                      gps, 
-                      proj, 
-                      conv_refiner, 
-                      detach=True, 
-                      scales=["16", "8", "4", "2", "1"], 
-                      displacement_dropout_p = displacement_dropout_p,
-                      gm_warp_dropout_p = gm_warp_dropout_p)
-    h,w = resolutions[resolution]
+    decoder = Decoder(coordinate_decoder,
+                      gps,
+                      proj,
+                      conv_refiner,
+                      detach=True,
+                      scales=["16", "8", "4", "2", "1"],
+                      displacement_dropout_p=displacement_dropout_p,
+                      gm_warp_dropout_p=gm_warp_dropout_p)
+    h, w = resolutions[resolution]
     encoder = CNNandDinov2(
-        cnn_kwargs = dict(
+        cnn_kwargs=dict(
             pretrained=pretrained_backbone,
-            amp = True),
-        amp = True,
-        use_vgg = True,
+            amp=True),
+        amp=True,
+        use_vgg=True,
     )
-    matcher = RegressionMatcher(encoder, decoder, h=h, w=w,**kwargs)
+    matcher = RegressionMatcher(encoder, decoder, h=h, w=w, **kwargs)
 
     if checkpoint_path is not None:
         print(f"Loading checkpoint from {checkpoint_path}")
@@ -309,7 +309,7 @@ def train(args):
     batch_size = args.gpu_batch_size
     step_size = gpus*batch_size
     romatch.STEP_SIZE = step_size
-    
+
     N = (32 * 250000)  # 250k steps of batch size 32
     # checkpoint every
     k = 25000 // romatch.STEP_SIZE
@@ -331,20 +331,20 @@ def train(args):
     mega_ws = mega.weight_scenes(megadepth_train, alpha=0.75)
     # Loss and optimizer
     depth_loss = RobustLosses(
-        ce_weight=0.01, 
-        local_dist={1:4, 2:4, 4:8, 8:8},
+        ce_weight=0.01,
+        local_dist={1: 4, 2: 4, 4: 8, 8: 8},
         local_largest_scale=8,
         depth_interpolation_mode=depth_interpolation_mode,
-        alpha = 0.5,
-        c = 1e-4,)
+        alpha=0.5,
+        c=1e-4, )
     parameters = [
         {"params": model.encoder.parameters(), "lr": romatch.STEP_SIZE * 5e-6 / 8},
         {"params": model.decoder.parameters(), "lr": romatch.STEP_SIZE * 1e-4 / 8},
     ]
     optimizer = torch.optim.AdamW(parameters, weight_decay=0.01)
     lr_scheduler = torch.optim.lr_scheduler.MultiStepLR(
-        optimizer, milestones=[(9*N/romatch.STEP_SIZE)//10])
-    megadense_benchmark = MegadepthDenseBenchmark("data/megadepth", num_samples = 1000, h=h,w=w)
+        optimizer, milestones=[int((9 * N / romatch.STEP_SIZE) // 10)])
+    megadense_benchmark = MegadepthDenseBenchmark("data/megadepth", num_samples=1000, h=h, w=w)
     checkpointer = CheckPoint(checkpoint_dir, experiment_name)
     model, optimizer, lr_scheduler, global_step = checkpointer.load(model, optimizer, lr_scheduler, global_step)
     romatch.GLOBAL_STEP = global_step
@@ -367,40 +367,43 @@ def train(args):
             n, k, mega_dataloader, ddp_model, depth_loss, optimizer, lr_scheduler, grad_scaler, grad_clip_norm = grad_clip_norm,
         )
         checkpointer.save(model, optimizer, lr_scheduler, romatch.GLOBAL_STEP)
-        wandb.log(megadense_benchmark.benchmark(model), step = romatch.GLOBAL_STEP)
+        wandb.log(megadense_benchmark.benchmark(model), step=romatch.GLOBAL_STEP)
 
 def test_mega_8_scenes(model, name):
     mega_8_scenes_benchmark = MegaDepthPoseEstimationBenchmark("data/megadepth",
-                                                scene_names=['mega_8_scenes_0019_0.1_0.3.npz',
-                                                    'mega_8_scenes_0025_0.1_0.3.npz',
-                                                    'mega_8_scenes_0021_0.1_0.3.npz',
-                                                    'mega_8_scenes_0008_0.1_0.3.npz',
-                                                    'mega_8_scenes_0032_0.1_0.3.npz',
-                                                    'mega_8_scenes_1589_0.1_0.3.npz',
-                                                    'mega_8_scenes_0063_0.1_0.3.npz',
-                                                    'mega_8_scenes_0024_0.1_0.3.npz',
-                                                    'mega_8_scenes_0019_0.3_0.5.npz',
-                                                    'mega_8_scenes_0025_0.3_0.5.npz',
-                                                    'mega_8_scenes_0021_0.3_0.5.npz',
-                                                    'mega_8_scenes_0008_0.3_0.5.npz',
-                                                    'mega_8_scenes_0032_0.3_0.5.npz',
-                                                    'mega_8_scenes_1589_0.3_0.5.npz',
-                                                    'mega_8_scenes_0063_0.3_0.5.npz',
-                                                    'mega_8_scenes_0024_0.3_0.5.npz'])
+                                                               scene_names=['mega_8_scenes_0019_0.1_0.3.npz',
+                                                                            'mega_8_scenes_0025_0.1_0.3.npz',
+                                                                            'mega_8_scenes_0021_0.1_0.3.npz',
+                                                                            'mega_8_scenes_0008_0.1_0.3.npz',
+                                                                            'mega_8_scenes_0032_0.1_0.3.npz',
+                                                                            'mega_8_scenes_1589_0.1_0.3.npz',
+                                                                            'mega_8_scenes_0063_0.1_0.3.npz',
+                                                                            'mega_8_scenes_0024_0.1_0.3.npz',
+                                                                            'mega_8_scenes_0019_0.3_0.5.npz',
+                                                                            'mega_8_scenes_0025_0.3_0.5.npz',
+                                                                            'mega_8_scenes_0021_0.3_0.5.npz',
+                                                                            'mega_8_scenes_0008_0.3_0.5.npz',
+                                                                            'mega_8_scenes_0032_0.3_0.5.npz',
+                                                                            'mega_8_scenes_1589_0.3_0.5.npz',
+                                                                            'mega_8_scenes_0063_0.3_0.5.npz',
+                                                                            'mega_8_scenes_0024_0.3_0.5.npz'])
     mega_8_scenes_results = mega_8_scenes_benchmark.benchmark(model, model_name=name)
     print(mega_8_scenes_results)
     json.dump(mega_8_scenes_results, open(f"results/mega_8_scenes_{name}.json", "w"))
+
 
 def test_mega1500(model, name):
     mega1500_benchmark = MegaDepthPoseEstimationBenchmark("data/megadepth")
     mega1500_results = mega1500_benchmark.benchmark(model, model_name=name)
     json.dump(mega1500_results, open(f"results/mega1500_{name}.json", "w"))
 
+
 def test_mega_dense(model, name):
-    megadense_benchmark = MegadepthDenseBenchmark("data/megadepth", num_samples = 1000)
+    megadense_benchmark = MegadepthDenseBenchmark("data/megadepth", num_samples=1000)
     megadense_results = megadense_benchmark.benchmark(model)
     json.dump(megadense_results, open(f"results/mega_dense_{name}.json", "w"))
-    
+
+
 def test_hpatches(model, name):
     hpatches_benchmark = HpatchesHomogBenchmark("data/hpatches")
     hpatches_results = hpatches_benchmark.benchmark(model)
@@ -408,13 +411,13 @@ def test_hpatches(model, name):
 
 
 if __name__ == "__main__":
-    os.environ["TORCH_CUDNN_V8_API_ENABLED"] = "1" # For BF16 computations
+    os.environ["TORCH_CUDNN_V8_API_ENABLED"] = "1"  # For BF16 computations
     os.environ["OMP_NUM_THREADS"] = "16"
-    torch.backends.cudnn.allow_tf32 = True # allow tf32 on cudnn
+    torch.backends.cudnn.allow_tf32 = True  # allow tf32 on cudnn
     import romatch
     parser = ArgumentParser()
     parser.add_argument("--only_test", action='store_true')
-    parser.add_argument("--debug_mode", action='store_true')
+    parser.add_argument("--debug_mode", action='store_true', default=True)
     parser.add_argument("--dont_log_wandb", action='store_true')
     parser.add_argument("--train_resolution", default='medium')
     parser.add_argument("--gpu_batch_size", default=8, type=int)
