f7fd266bb93ba7fbda2d2df12a411bbf168b353e
tjelinek <tomasjelinek96@gmail.com>
2025-05-27 11:27:49 +0200

    adding HO3D dataset

--- romatch/datasets/ho3d.py
+++ romatch/datasets/ho3d.py
@@ -11,28 +11,28 @@ from romatch.utils import *
 import math
 
 
-class BopScene:
+class HO3DScene:
     def __init__(
-        self,
-        data_root,
-        scene_info,
-        ht=384,
-        wt=512,
-        min_overlap=0.0,
-        max_overlap=1.0,
-        shake_t=0,
-        normalize=True,
-        max_num_pairs = 100_000,
-        scene_name = None,
-        use_horizontal_flip_aug = False,
-        use_single_horizontal_flip_aug = False,
-        colorjiggle_params = None,
-        random_eraser = None,
-        use_randaug = False,
-        randomize_size = False,
+            self,
+            data_root,
+            scene_info,
+            ht=384,
+            wt=512,
+            min_overlap=0.0,
+            max_overlap=1.0,
+            shake_t=0,
+            normalize=True,
+            max_num_pairs=100_000,
+            scene_name=None,
+            use_horizontal_flip_aug=False,
+            use_single_horizontal_flip_aug=False,
+            colorjiggle_params=None,
+            random_eraser=None,
+            use_randaug=False,
+            randomize_size=False,
     ) -> None:
         self.data_root = data_root
-        self.scene_name = os.path.splitext(scene_name)[0]+f"_{min_overlap}_{max_overlap}"
+        self.scene_name = os.path.splitext(scene_name)[0] + f"_{min_overlap}_{max_overlap}"
         self.image_paths = scene_info["image_paths"]
         self.depth_paths = scene_info["depth_paths"]
         self.intrinsics = scene_info["intrinsics"]
@@ -50,11 +50,11 @@ class BopScene:
             self.overlaps = self.overlaps[pairinds]
         if randomize_size:
             area = ht * wt
-            s = int(16 * (math.sqrt(area)//16))
-            sizes = ((ht,wt), (s,s), (wt,ht))
+            s = int(16 * (math.sqrt(area) // 16))
+            sizes = ((ht, wt), (s, s), (wt, ht))
             choice = romatch.RANK % 3
-            ht, wt = sizes[choice] 
-        # counts, bins = np.histogram(self.overlaps,20)
+            ht, wt = sizes[choice]
+            # counts, bins = np.histogram(self.overlaps,20)
         # print(counts)
         self.im_transform_ops = get_tuple_transform_ops(
             resize=(ht, wt), normalize=normalize, colorjiggle_params=colorjiggle_params,
@@ -72,17 +72,17 @@ class BopScene:
     def load_im(self, im_path):
         im = Image.open(im_path)
         return im
-    
-    def horizontal_flip(self, im_A, im_B, depth_A, depth_B,  K_A, K_B):
+
+    def horizontal_flip(self, im_A, im_B, depth_A, depth_B, K_A, K_B):
         im_A = im_A.flip(-1)
         im_B = im_B.flip(-1)
-        depth_A, depth_B = depth_A.flip(-1), depth_B.flip(-1) 
-        flip_mat = torch.tensor([[-1, 0, self.wt],[0,1,0],[0,0,1.]]).to(K_A.device)
-        K_A = flip_mat@K_A  
-        K_B = flip_mat@K_B  
-        
+        depth_A, depth_B = depth_A.flip(-1), depth_B.flip(-1)
+        flip_mat = torch.tensor([[-1, 0, self.wt], [0, 1, 0], [0, 0, 1.]]).to(K_A.device)
+        K_A = flip_mat @ K_A
+        K_B = flip_mat @ K_B
+
         return im_A, im_B, depth_A, depth_B, K_A, K_B
-    
+
     def load_depth(self, depth_ref, crop=None):
 
         depth = cv2.imread(str(depth_ref), cv2.IMREAD_UNCHANGED)  # uint16
@@ -114,8 +114,8 @@ class BopScene:
         T1 = self.poses[idx1]
         T2 = self.poses[idx2]
         T_1to2 = torch.tensor(np.matmul(T2, np.linalg.inv(T1)), dtype=torch.float)[
-            :4, :4
-        ]  # (4, 4)
+                 :4, :4
+                 ]  # (4, 4)
 
         # Load positive pair data
         im_A, im_B = self.image_paths[idx1], self.image_paths[idx2]
@@ -139,16 +139,16 @@ class BopScene:
         depth_A, depth_B = self.depth_transform_ops(
             (depth_A[None, None], depth_B[None, None])
         )
-        
+
         [im_A, im_B, depth_A, depth_B], t = self.rand_shake(im_A, im_B, depth_A, depth_B)
         K1[:2, 2] += t
         K2[:2, 2] += t
-        
+
         im_A, im_B = im_A[None], im_B[None]
         if self.random_eraser is not None:
             im_A, depth_A = self.random_eraser(im_A, depth_A)
             im_B, depth_B = self.random_eraser(im_B, depth_B)
-                
+
         if self.use_horizontal_flip_aug:
             if np.random.rand() > 0.5:
                 im_A, im_B, depth_A, depth_B, K1, K2 = self.horizontal_flip(im_A, im_B, depth_A, depth_B, K1, K2)
@@ -158,10 +158,10 @@ class BopScene:
 
         if romatch.DEBUG_MODE:
             tensor_to_pil(im_A[0], unnormalize=True).save(
-                            f"vis/im_A.jpg")
+                f"vis/im_A.jpg")
             tensor_to_pil(im_B[0], unnormalize=True).save(
-                            f"vis/im_B.jpg")
-            
+                f"vis/im_B.jpg")
+
         data_dict = {
             "im_A": im_A[0],
             "im_A_identifier": self.image_paths[idx1].split("/")[-1].split(".jpg")[0],
@@ -174,12 +174,12 @@ class BopScene:
             "T_1to2": T_1to2,
             "im_A_path": im_A_ref,
             "im_B_path": im_B_ref,
-            
+
         }
         return data_dict
 
 
-class BOPBuilder:
+class HO3DBuilder:
     def __init__(self, data_root=Path("/mnt/personal/jelint19/data/bop")) -> None:
         self.data_root: Path = data_root
 
@@ -211,14 +211,18 @@ class BOPBuilder:
         for scene_name in sorted(scene_names):
             scene_info_path = path_to_scenes / scene_name / 'scene_info.npy'
 
+            if not scene_info_path.exists():
+                continue
+
             scene_info = np.load(scene_info_path, allow_pickle=True).item()
 
-            scenes.append(BopScene(self.data_root, scene_info, min_overlap=min_overlap, scene_name=scene_name, **kwargs))
+            scenes.append(
+                BopScene(self.data_root, scene_info, min_overlap=min_overlap, scene_name=scene_name, **kwargs))
         return scenes
 
     def weight_scenes(self, concat_dataset, alpha=0.5):
         ns = []
         for d in concat_dataset.datasets:
             ns.append(len(d))
-        ws = torch.cat([torch.ones(n) / n**alpha for n in ns])
+        ws = torch.cat([torch.ones(n) / n ** alpha for n in ns])
         return ws
